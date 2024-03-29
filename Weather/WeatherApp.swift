824a4af8c819bf2779c8aa56619416e5a5a70433
mohamed.khater <m.khater@epicsyst.com>
2024-03-29 14:36:51 +0200

    initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Epic_Sys on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
